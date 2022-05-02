bc8f0dd9982404a0f1cc5a0ab159c3a47ecf99c3
su <abcc0785@gmail.com>
2022-05-02 14:13:48 +0900

    [ADD] Interior post create

--- server/routes/Interiors.js
+++ server/routes/Interiors.js
@@ -5,17 +5,20 @@ const interiorController = require('../controllers/InteriorController');
 const likeController = require('../controllers/LikeController');
 const commentController = require('../controllers/CommentController');
 const isAuth = require('../middlewares/Authentication');
+const imageHandler = require('../middlewares/imageHandler');
 
-//게시글 조회 수정 삭제
+//게시글 조회
 router.get('/:id', isAuth, interiorController.get);
-// router.patch('/:id', isAuth, interiorController.patch);
-// router.delete('/:id', isAuth, interiorController.delete);
+//수정
+router.patch('/:id', isAuth, interiorController.patch, imageHandler.patch);
+//삭제
+router.delete('/:id', isAuth, interiorController.delete, imageHandler.delete);
 
 //좋아요
 router.post('/:id/likes', isAuth, likeController.post);
 router.delete('/:id/likes', isAuth, likeController.delete);
 
-// //댓글 작성
+//댓글 작성
 router.post('/:id/comments', isAuth, commentController.post);
 
 module.exports = router;
--- server/routes/Plants.js
+++ server/routes/Plants.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const plantController = require('../controllers/PlantController');
-// const interiorController = require('../controllers/InteriorController');
+const interiorController = require('../controllers/InteriorController');
+const isAuth = require('../middlewares/Authentication');
+const { post } = require('../middlewares/imageHandler');
 
+//식물 상세정보 조회
 router.get('/:id', plantController.get);
-// router.post('/:id/interiors', interiorController.post);
+
+//게시글 작성
+router.post(
+  '/:id/interiors',
+  isAuth,
+  post.single('image'), //클라 단에서 설정한 폼데이터의 키 이름 쓴다
+  interiorController.post,
+);
 
 module.exports = router;
